b7db8b1af5b9b5931c6af36d675ed2cf35125274
Jannik Keye <jannik.keye@gmail.com>
2019-02-09 20:42:49 +0100

    feat: repl

---
+++ interpreter/src/repl.rs
@@ -0,0 +1,23 @@
+use crate::lexer;
+use std::io::{Write, stdin, stdout};
+
+const PROMPT: &'static str = ">>";
+
+pub fn start() {
+    println!("
+
+    ");
+    loop {
+        let mut input = String::new();
+        print!("{}", PROMPT);
+
+        stdout().flush().expect("Error flushing stdout");
+        stdin().read_line(&mut input).expect("Error reading from STDIN");
+
+        let lexer = lexer::Lexer::new(&input);
+
+        for token in lexer {
+            println!("{:?}", token);
+        }
+    }
+}
---
+++ src/bin/repl.rs
@@ -0,0 +1,7 @@
+extern crate interpreter;
+
+use interpreter::repl;
+
+fn main() {
+    repl::start();
+}
